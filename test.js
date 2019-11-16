5095dc94a46cc1dc0b1dd75001afd0dfeaa7ccf9
Welkson Ramos <welksonr@hotmail.com>
2019-11-15 23:25:51 -0300

    fix test :white_check_mark:

--- test.js
+++ test.js
@@ -3,12 +3,12 @@
 const desmotivacionais = require('./');
 const test = require('tape');
 
-test('Return a random word', function(t){
+test('Return a random word', (t) => {
 	t.ok(desmotivacionais.random(), 'check if return one word');
 	t.end();
 });
 
-test('Return all words', function(t){
+test('Return all words', (t) => {
 	t.ok(Array.isArray(desmotivacionais.all), 'Check if words is array');
 	t.ok(desmotivacionais.all[1].length,'Check if is content');
 	t.end();
