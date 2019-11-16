0828b99c81c50d287f083e50118f7f5c37e9f9d0
Welkson Ramos <welksonr@hotmail.com>
2019-11-15 23:33:47 -0300

    rewrite :recycle:

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var desmotivacionais = require('./desmotivacionais.json'),
-uniqueRandomArray = require('unique-random-array');
+// const desmotivacionais = require('./desmotivacionais.json');
+const desmotivacionais = require('./quotes');
+const uniqueRandomArray = require('unique-random-array');
 
 module.exports.all = desmotivacionais;
 module.exports.random = uniqueRandomArray(desmotivacionais);
