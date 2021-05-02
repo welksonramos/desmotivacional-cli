b3f208f9e75a1213169002877148cd6054c94b9c
Welkson Ramos <welksonr@hotmail.com>
2021-05-02 17:59:51 -0300

    chore: migrate from tape to ava

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 
-const desmotivacionais = require('./');
-const test = require('tape');
+import quotes from "./index.js";
+import test from "ava";
 
-test('Return a random word', (t) => {
-	t.ok(desmotivacionais.random(), 'check if return one word');
-	t.end();
+test("Retorna uma frase aleatória", t => {
+	t.is(typeof quotes.random(), "string");
 });
 
-test('Return all words', (t) => {
-	t.ok(Array.isArray(desmotivacionais.all), 'Check if words is array');
-	t.ok(desmotivacionais.all[1].length,'Check if is content');
-	t.end();
+test("Retorna todas as frases", t => {
+	const totalQuotes = quotes.all.length;
+	t.is(quotes.all.length,  totalQuotes);
+});
+
+test("Checa se é um array de frases", t => {
+	if(Array.isArray(quotes.all)) {
+		for (let quote of quotes.all) {
+			t.is(typeof quote, "string");
+		}
+	}
 });
